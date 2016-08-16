31d32dbae2f041158aa92fd94d4e238327ad43f5
PeteyChan <akivsfyuu@gmail.com>
2016-08-16 23:55:36 +1000

    Add files via upload
    Initial Commit

---
+++ GameObjectPool.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+using ECS.Internal;
+
+namespace ECS
+{
+	public static class GameObjectPool
+	{
+		static Dictionary<string, ObjectPool> _pools = new Dictionary<string, ObjectPool>();
+
+		static Transform _p;
+		static Transform _parent
+		{
+			get
+			{
+				if (_p == null)
+				{
+					_p = new GameObject("Object Pool").transform;
+				}
+				return _p;
+			}
+		}
+
+		public static void Pool(this GameObject obj)
+		{
+			ObjectPool pool;
+			obj.transform.parent = _parent;
+			if (_pools.TryGetValue(obj.name, out pool) == false)
+			{
+				pool = new ObjectPool();
+				_pools.Add(obj.name, pool);	
+			}
+			pool.PoolObject(obj);
+		}
+
+
+		//
+		//	GET OBJECT METHODS
+		//
+
+		public static GameObject Get(GameObject go)
+		{
+			return Get(go.name);
+		}
+
+		public static GameObject Get(GameObject go, Transform parent)
+		{
+			return Get(go.name, parent);
+		}
+
+		public static GameObject Get(string path)
+		{
+			return Get (path, _parent);
+		}
+
+		/// <summary>
+		/// Returns Prefab from path and sets it up using method
+		/// </summary>
+		public static GameObject Get(string path, Transform parent)
+		{
+			#if UNITY_EDITOR
+			if (string.IsNullOrEmpty(path))
+				Debug.LogError("Must provide a path to object file");
+			#endif
+
+			ObjectPool pool;
+			if (_pools.TryGetValue(path, out pool) == false)
+			{
+				pool = new ObjectPool();
+				_pools.Add(path, pool);
+			}
+			GameObject go = pool.GetObject(path);
+			go.transform.parent = parent;
+			go.SetActive(true);
+			return go;
+		}
+	}	
+}
+
+namespace ECS.Internal
+{
+
+	public class ObjectPool
+	{
+		Stack<GameObject> _pool = new Stack<GameObject>();
+
+		public void PoolObject(GameObject go)
+		{
+			go.SetActive(false);
+			_pool.Push(go);
+		}
+
+		public GameObject GetObject(string path)
+		{
+			GameObject go;
+			if (_pool.Count > 0)
+			{
+				go = _pool.Pop();
+			}
+			else
+			{
+				go = Resources.Load(path) as GameObject;
+				go = GameObject.Instantiate(go) as GameObject;
+				go.name = path;
+			}
+			return go;
+		}
+	}
+}
+
---
+++ Groups.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using ECS.Internal;
+using System;
+
+namespace ECS
+{
+	public abstract class Groups	// To allow dictionary use
+	{
+		// reference to all groups
+		static Dictionary<Type, Groups> _groups
+		{
+			get {return EntityPool._groups;}
+		}
+
+		/// <summary>
+		/// Fires Event when Component is Added.
+		/// Returns associated Entity.
+		/// </summary>
+		public static void AddComponentEvent<C>(ComponentEvent listener, bool subscribe) where C : EntityComponent, new()
+		{
+			if(subscribe)
+				ComponentPool<C>.AddComponentEvent += listener;
+			else
+				ComponentPool<C>.AddComponentEvent -= listener;
+		}
+
+		/// <summary>
+		/// Fires Event when Component is Removed.
+		/// Returns associated Entity.
+		/// </summary>
+		public static void RemoveComponentEvent<C>(ComponentEvent listener, bool subscribe) where C: EntityComponent, new()
+		{
+			if (subscribe)
+				ComponentPool<C>.RemoveComponentEvent += listener;
+			else
+				ComponentPool<C>.RemoveComponentEvent -= listener;
+		}
+
+		/// <summary>
+		/// Gets Group that allows processing of all entities that have the component.
+		/// </summary>
+		public static Group<C> GetGroup<C>() where C: EntityComponent, new()
+		{
+			Groups lookUpGroup;
+			if (_groups.TryGetValue(typeof(Group<C>), out lookUpGroup))
+			{
+				return lookUpGroup as Group<C>;
+			}
+			Group<C> newGroup = new Group<C>();
+			_groups.Add(typeof(Group<C>), newGroup);
+			return newGroup;
+		}
+
+		/// <summary>
+		/// Gets Group that allows processing of all entities that have all components.
+		/// </summary>
+		public static Group<C1, C2> GetGroup<C1, C2>()
+			where C1: EntityComponent, new()
+			where C2: EntityComponent, new()
+		{
+			Groups lookUpGroup;
+			if (_groups.TryGetValue(typeof(Group<C1, C2>), out lookUpGroup))
+			{
+				return lookUpGroup as Group<C1, C2>;
+			}
+			Group<C1, C2> newGroup = new Group<C1, C2>();
+			_groups.Add(newGroup.GetType(), newGroup);
+			return newGroup;
+		}
+
+
+		/// <summary>
+		/// Gets Group that allows processing of all entities that have all components.
+		/// </summary>
+		public static Group<C1, C2, C3> GetGroup<C1, C2, C3>()
+			where C1: EntityComponent, new()
+			where C2: EntityComponent, new()
+			where C3: EntityComponent, new()
+		{
+			Groups lookUpGroup;
+			if (_groups.TryGetValue(typeof(Group<C1, C2, C3>), out lookUpGroup))
+			{
+				return lookUpGroup as Group<C1, C2, C3>;
+			}
+			Group<C1, C2, C3> newGroup = new Group<C1, C2, C3>();
+			_groups.Add(newGroup.GetType(), newGroup);
+			return newGroup;
+		}
+
+		/// <summary>
+		/// Gets Group that allows processing of all entities that have all components.
+		/// </summary>
+		public static Group<C1, C2, C3, C4> GetGroup<C1, C2, C3, C4>()
+			where C1: EntityComponent, new()
+			where C2: EntityComponent, new()
+			where C3: EntityComponent, new()
+			where C4: EntityComponent, new()
+		{
+			Groups lookUpGroup;
+			if (_groups.TryGetValue(typeof(Group<C1, C2, C3, C4>), out lookUpGroup))
+			{
+				return lookUpGroup as Group<C1, C2, C3, C4>;
+			}
+			Group<C1, C2, C3, C4> newGroup = new Group<C1, C2, C3, C4>();
+			_groups.Add(newGroup.GetType(), newGroup);
+			return newGroup;
+		}
+
+		/// <summary>
+		/// Returns the component ID of Component
+		/// </summary>
+		public static int GetComponentID<C>() where C : EntityComponent, new()
+		{
+			return EntityPool.GetComponentID<C>();
+		}
+	}
+
+}
